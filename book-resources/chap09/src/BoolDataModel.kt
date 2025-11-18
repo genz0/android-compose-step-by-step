89dcbdb06dc63646bb421738f0d76d3884647981
GENZO <genzo1300@gmail.com>
2025-11-18 14:29:42 +0900

    リソース追加

---
+++ book-resources/chap09/src/BoolDataModel.kt
@@ -0,0 +1,41 @@
+package com.example.practicebooksearchapp
+
+const val sampleJSON = """
+{
+  "items": [
+    {
+      "volumeInfo": {
+        "title": "書籍-1",
+        "authors": ["著者-1"],
+        "description": "",
+        "imageLinks": {
+          "smallThumbnail": "smallThumbnail-1",
+          "thumbnail": "thumbnail-1"
+        }
+      }
+    },
+    {
+      "volumeInfo": {
+        "title": "書籍-2",
+        "authors": ["著者-2"],
+        "description": "",
+        "imageLinks": {
+          "smallThumbnail": "smallThumbnail-2",
+          "thumbnail": "thumbnail-2"
+        }
+      }
+    },
+    {
+      "volumeInfo": {
+        "title": "書籍-3",
+        "authors": ["著者-3"],
+        "description": "",
+        "imageLinks": {
+          "smallThumbnail": "smallThumbnail-3",
+          "thumbnail": "thumbnail-3"
+        }
+      }
+    }
+  ]
+}
+"""
