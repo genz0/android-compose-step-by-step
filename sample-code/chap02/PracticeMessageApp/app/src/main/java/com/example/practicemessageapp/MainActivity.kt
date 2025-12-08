7b2679393e0e07a7eedf1e27c994bea9f59ca05a
Matsuo <matsuo.ins@gmail.com>
2025-12-09 00:36:54 +0900

    chapter02 サンプルコードの追加

---
+++ sample-code/chap02/PracticeMessageApp/app/src/main/java/com/example/practicemessageapp/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.practicemessageapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practicemessageapp.ui.theme.PracticeMessageAppTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      PracticeMessageAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  val hello = remember { mutableStateOf("こんにちは") }
+  val context = LocalContext.current
+  Column(
+    modifier = modifier
+  ) {
+    Text(
+      text = "${hello.value} $name!",
+      fontSize = 32.sp, // フォントサイズを大きく
+      color = Color.Red, // テキストカラーを赤くする
+    )
+    Button(onClick = {
+      // ここに後で処理を追加する
+      hello.value = "こんばんは"
+    }) {
+      Text("夜")
+    } // Buttonの閉じカッコ
+
+    Button(onClick = {
+      hello.value = "おはよう"
+      Toast.makeText(context, "変更しました", Toast.LENGTH_SHORT).show()
+    }) {
+      Text("朝")
+    } // Buttonの閉じカッコ
+
+  }// Columnの閉じカッコ
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  PracticeMessageAppTheme {
+    Greeting("Android")
+  }
+}
