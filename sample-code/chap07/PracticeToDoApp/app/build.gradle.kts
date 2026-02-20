5e870ee21aa8015f8a016c3d768579395d820803
GENZO <genzo1300@gmail.com>
2026-02-20 12:25:50 +0900

    chapter7 android studio panda2対応

--- sample-code/chap07/PracticeToDoApp/app/build.gradle.kts
+++ sample-code/chap07/PracticeToDoApp/app/build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
   alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
   // Kotlin 2系に対応したKSP
- id("com.google.devtools.ksp") version "2.0.21-1.0.28"
+  //id("com.google.devtools.ksp") version "2.0.21-1.0.28"
+  id("com.google.devtools.ksp") version "2.3.5" // Kotlin 2系 AndroidStudio Pandaに対応したKSP
+
 }
 
 android {
   namespace = "com.example.practicetodoapp"
   compileSdk {
-    version = release(36)
+    version = release(36) {
+      minorApiLevel = 1
+    }
   }
 
   defaultConfig {
@@ -25,15 +28,15 @@ android {
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
   }
   buildFeatures {
     compose = true
