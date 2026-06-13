d1b2b7ad2e9cfc7c00022f1707b77ffd8d36e43f
GENZO <genzo1300@gmail.com>
2026-06-13 11:13:52 +0900

    chapter7 android studio quail1対応

--- sample-code/chap07/PracticeToDoApp/app/build.gradle.kts
+++ sample-code/chap07/PracticeToDoApp/app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
   // Kotlin 2系に対応したKSP
   //id("com.google.devtools.ksp") version "2.0.21-1.0.28"
   id("com.google.devtools.ksp") version "2.3.6" // Kotlin 2系 AndroidStudio Pandaに対応したKSP
-
 }
 
 android {
@@ -27,8 +26,9 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      optimization {
+        enable = false
+      }
     }
   }
   compileOptions {
@@ -48,19 +48,19 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
   implementation("androidx.compose.material:material-icons-extended") // material-icons-extended
 
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
   testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  debugImplementation(libs.androidx.compose.ui.tooling)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.junit)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
+  debugImplementation(libs.androidx.compose.ui.tooling)
 }
