f3d0b19d701a97374d1c4bddef5e73f3cccd4ec5
GENZO <genzo1300@gmail.com>
2026-06-13 11:35:14 +0900

    chapter8 android studio quail1対応
    ・CameraX 1.6.0　→　1.6.1
    ・navigation-compose:2.9.7 →　2.9.8

--- sample-code/chap08/PracticeCameraApp/app/build.gradle.kts
+++ sample-code/chap08/PracticeCameraApp/app/build.gradle.kts
@@ -23,8 +23,9 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      optimization {
+        enable = false
+      }
     }
   }
   compileOptions {
@@ -41,28 +42,28 @@ dependencies {
   implementation("com.google.mlkit:barcode-scanning:17.3.0") // ML Kit barcode-scanning
   implementation("com.google.mlkit:text-recognition-japanese:16.0.1") // ML Kit text-recognition
 
-  implementation("androidx.camera:camera-camera2:1.6.0")  // CameraX
-  implementation("androidx.camera:camera-core:1.6.0")
-  implementation("androidx.camera:camera-lifecycle:1.6.0")
-  implementation("androidx.camera:camera-view:1.6.0")
+  implementation("androidx.camera:camera-camera2:1.6.1")  // CameraX
+  implementation("androidx.camera:camera-core:1.6.1")
+  implementation("androidx.camera:camera-lifecycle:1.6.1")
+  implementation("androidx.camera:camera-view:1.6.1")
   implementation("androidx.compose.material:material-icons-extended") // material-icons-extended
 
   implementation("com.google.accompanist:accompanist-permissions:0.37.3") // Permissions
-  implementation("androidx.navigation:navigation-compose:2.9.7") // Navigation
+  implementation("androidx.navigation:navigation-compose:2.9.8") // Navigation
 
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
   testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  debugImplementation(libs.androidx.compose.ui.tooling)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.junit)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
+  debugImplementation(libs.androidx.compose.ui.tooling)
 }
