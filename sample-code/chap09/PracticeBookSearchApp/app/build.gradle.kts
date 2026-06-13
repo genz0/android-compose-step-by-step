b18434109c348a35cb1c71524cbb94685414e5ca
GENZO <genzo1300@gmail.com>
2026-06-13 12:59:44 +0900

    chapter9 android studio quail1対応
    ・gson:2.13.2　→　2.14.0
    ・navigation-compose:2.9.7　→　2.9.8

--- sample-code/chap09/PracticeBookSearchApp/app/build.gradle.kts
+++ sample-code/chap09/PracticeBookSearchApp/app/build.gradle.kts
@@ -23,8 +23,9 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      optimization {
+        enable = false
+      }
     }
   }
   compileOptions {
@@ -38,24 +39,24 @@ android {
 
 dependencies {
   implementation("io.coil-kt.coil3:coil-compose:3.4.0")
-  implementation("com.google.code.gson:gson:2.13.2")
+  implementation("com.google.code.gson:gson:2.14.0")
   implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
-  implementation("androidx.navigation:navigation-compose:2.9.7")
+  implementation("androidx.navigation:navigation-compose:2.9.8")
 
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
   testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  debugImplementation(libs.androidx.compose.ui.tooling)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.junit)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
+  debugImplementation(libs.androidx.compose.ui.tooling)
 }
